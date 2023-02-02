7324901f33e2eb1d354cbd30b261603ce96f8758
Anuj Kumar Pandey <83698322+codermal7@users.noreply.github.com>
2023-02-02 22:47:32 +0530

    Time: 10 ms (30.58%), Space: 9.7 MB (29.72%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int t) {
+        int n=nums.size();
+        int s=0,e=n-1,mid;
+        while(s<=e)
+        {
+            mid=s+((e-s)/2);
+            if(t==nums[mid])
+                return mid;
+            else if(t>nums[mid])
+                s=mid+1;
+            else
+                e=mid-1;
+        }
+        return s;
+        
+    }
+};
