5e8ba293ad8f69f8c9ae8dd840bdf8d28c7b94c7
Anuj Kumar Pandey <83698322+codermal7@users.noreply.github.com>
2023-02-25 20:23:06 +0530

    Time: 27 ms (60.47%), Space: 15 MB (32.50%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,41 +1,31 @@
 class Solution {
     private:
-    // void dfs(int node,vector<int> adj[], int vis[])
-    // {
-    //   vis[node]=1;
-    //   for(auto it:adj[node])
-    //   {
-    //       if(!vis[it])
-    //       {
-    //         dfs(it,adj,vis);
-    //       }
-    //   }
-    // }
-    void dfs(int V,int node,vector<vector<int>>& adj, int vis[])
+    void dfs(int node,vector<int> adj[], int vis[])
     {
-        int m=V;
       vis[node]=1;
-        for(int i=0;i<m;i++)
-          if(adj[node][i]==1 && !vis[i])
+      for(auto it:adj[node])
+      {
+          if(!vis[it])
           {
-            dfs(m,i,adj,vis);
+            dfs(it,adj,vis);
           }
       }
+    }
     public:
     int findCircleNum(vector<vector<int>>& isConnected) {
         int V=isConnected.size();
-        vector<int> ad[V+1];int c=0;
-        // for(int i=1;i<V;i++)
-        // {
-        //     for(int j=1;j<V;j++)
-        //     {
-        //         if(isConnected[i][j]==1 && i!=j)
-        //         {
-        //             ad[i].push_back(j);
-        //             // ad[j].push_back(i);
-        //         }
-        //     }
-        // }
+        vector<int> ad[V];int c=0;
+        for(int i=0;i<V;i++)
+        {
+            for(int j=0;j<V;j++)
+            {
+                if(isConnected[i][j]==1 && i!=j)
+                {
+                    ad[i].push_back(j);
+                    ad[j].push_back(i);
+                }
+            }
+        }
         int vis[V];
         memset(vis,0,V*sizeof(int));
         for(int i=0;i<V;i++)
@@ -43,7 +33,7 @@ class Solution {
             if(!vis[i])
             {
                 c++;
-                dfs(V,i,isConnected,vis);
+                dfs(i,ad,vis);
             }
         }
         return c;    
