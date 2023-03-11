580c5ce99032e53dcfbcb0d0c9719aebeff672d4
Anuj Kumar Pandey <83698322+codermal7@users.noreply.github.com>
2023-03-12 00:22:24 +0530

    Added solution - LeetHub

--- Count ways to reach the n'th stair - GFG/count-ways-to-reach-the-nth-stair.cpp
+++ Count ways to reach the n'th stair - GFG/count-ways-to-reach-the-nth-stair.cpp
@@ -7,28 +7,29 @@ class Solution
 {
     public:
     long long int m =1000000007;
-    // int disp(vector<int> &dp,int n)
-    // {
-    //     if(n<=1)
-    //         return 1;
-    //     if(dp[n]!=-1)
-    //         return dp[n];
-    //     return (dp[n]=disp(dp,n-1)+disp(dp,n-2));
-    // }
+    int disp(vector<int> &dp,int n)
+    {
+        if(n<=1)
+            return 1;
+        if(dp[n]!=-1)
+            return dp[n]%m;
+        return (dp[n]=(disp(dp,n-1)+disp(dp,n-2))%m);
+    }
     
     int countWays(int n)
     {
-        // vector<int> dp(n+1,-1);
-        if(n<=1)return 1;
+        vector<int> dp(n+1,-1);
+        // if(n<=1)return 1;
         
-        long a=1,b=1;long c;
-        for(int i=2;i<=n;i++)
-        {
-            c=(a+b)%m;
-            a=b;
-            b=c;
-        }
-        return c%m;
+        // long a=1,b=1;long c;
+        // for(int i=2;i<=n;i++)
+        // {
+        //     c=(a+b)%m;
+        //     a=b;
+        //     b=c;
+        // }
+        // return c%m;
+        return disp(dp,n);
     }
 };
 
