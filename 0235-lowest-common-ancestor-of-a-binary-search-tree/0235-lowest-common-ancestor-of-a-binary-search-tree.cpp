9e6bb8813fbe3d5323ae3adf2440fc198c2b6164
Anuj Kumar Pandey <83698322+codermal7@users.noreply.github.com>
2023-03-27 15:08:22 +0530

    Time: 32 ms (62.61%), Space: 23.3 MB (28.18%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -8,25 +8,25 @@
  * };
  */
 
-class Solution {
-public:
-    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if (p->val < root->val && q->val < root->val)
-            return lowestCommonAncestor(root->left, p, q);
-        else if (p->val > root->val && q->val > root->val)
-            return lowestCommonAncestor(root->right, p, q);
-        else return root;
-    }
-};
-
 // class Solution {
 // public:
 //     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-//         while (root) {
-//             if (p->val < root->val && q->val < root->val) root = root->left;
-//             else if (p->val > root->val && q->val > root->val) root = root->right;
-//             else return root;
-//         }
-//         return root;
+//         if (p->val < root->val && q->val < root->val)
+//             return lowestCommonAncestor(root->left, p, q);
+//         else if (p->val > root->val && q->val > root->val)
+//             return lowestCommonAncestor(root->right, p, q);
+//         else return root;
 //     }
 // };
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        while (root) {
+            if (p->val < root->val && q->val < root->val) root = root->left;
+            else if (p->val > root->val && q->val > root->val) root = root->right;
+            else return root;
+        }
+        return root;
+    }
+};
