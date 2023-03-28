4316e4105097b74cc92707457ad10e3502e429fd
Anuj Kumar Pandey <83698322+codermal7@users.noreply.github.com>
2023-03-28 20:04:13 +0530

    Time: 18 ms (92.04%), Space: 11.2 MB (50.85%) - LeetHub

--- 0589-n-ary-tree-preorder-traversal/0589-n-ary-tree-preorder-traversal.cpp
+++ 0589-n-ary-tree-preorder-traversal/0589-n-ary-tree-preorder-traversal.cpp
@@ -19,46 +19,48 @@ public:
 */
 
 // BY USING RECURSION
-class Solution {
-private:
-    void travel(Node* root, vector<int>& result) {
-        if (root == nullptr) {
-            return;
-        }
-        
-        result.push_back(root -> val);
-        for (Node* child : root -> children) {
-            travel(child, result);
-        }
-    }
-public:
-    vector<int> preorder(Node* root) {
-        vector<int> result;
-        travel(root, result);
-        return result;
-    }
-};
-
 // class Solution {
+// private:
+//     void travel(Node* root, vector<int>& result) {
+//         if (root == nullptr) {
+//             return;
+//         }
+        
+//         result.push_back(root -> val);
+//         for (Node* child : root -> children) {
+//             travel(child, result);
+//         }
+//     }
 // public:
 //     vector<int> preorder(Node* root) {
 //         vector<int> result;
-//         if (root == nullptr) {
-//             return result;
-//         }
-        
-//         stack<Node*> stk;
-//         stk.push(root);
-//         while (!stk.empty()) {
-//             Node* cur = stk.top();
-//             stk.pop();
-//             result.push_back(cur -> val);
-//             for (int i = cur -> children.size() - 1; i >= 0; i--) {
-//                 if (cur -> children[i] != nullptr) {
-//                     stk.push(cur -> children[i]);
-//                 }
-//             }
-//         }
+//         travel(root, result);
 //         return result;
 //     }
 // };
+
+
+// BY USING ITERATIVE APPROACH
+class Solution {
+public:
+    vector<int> preorder(Node* root) {
+        vector<int> result;
+        if (root == nullptr) {
+            return result;
+        }
+        
+        stack<Node*> stk;
+        stk.push(root);
+        while (!stk.empty()) {
+            Node* cur = stk.top();
+            stk.pop();
+            result.push_back(cur -> val);
+            for (int i = cur -> children.size() - 1; i >= 0; i--) {
+                if (cur -> children[i] != nullptr) {
+                    stk.push(cur -> children[i]);
+                }
+            }
+        }
+        return result;
+    }
+};
