6f65f0e3eae986fd54d56bfdbfc3f66167f75f4c
Anuj Kumar Pandey <83698322+codermal7@users.noreply.github.com>
2023-03-28 19:53:27 +0530

    Time: 450 ms (5.27%), Space: 253.3 MB (5.43%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int find(vector<int> &prices, int i,int k,bool buy,vector<vector<int>> &v)
+    {
+        //if we have no stock or we have no chance of transaction(k=0)
+        if(i>=prices.size()||k<=0) return 0;
+        if(v[i][buy]!=-1) return v[i][buy];
+        
+        if(buy)  //if we are buying then next time we will sell else next time we will buy
+        {        //-prices[i], because bought stock of prices[i], expend money
+            
+           return v[i][buy]=max(-prices[i]+find(prices,i+1,k,!buy,v),find(prices,i+1,k,buy,v)); 
+        }
+        else    //it's time to sell , now decrease k, we have done 1 transaction
+        {       //+prices[i], because we now gain (i.e) sell our stock at rate of prices[i]
+           return v[i][buy]=max( prices[i]+find(prices,i+1,k-1,!buy,v),find(prices,i+1,k,buy,v)); 
+        }
+    }
+    int maxProfit(vector<int>& prices) {
+        
+        int n=prices.size(); 
+        vector<vector<int>> v(n,vector<int> (21,-1));
+        //passing here buy=1 because we will first buy then sell
+        //we can do atmost k=1 transaction
+        return find(prices,0,1,1,v);
+    }
+};
