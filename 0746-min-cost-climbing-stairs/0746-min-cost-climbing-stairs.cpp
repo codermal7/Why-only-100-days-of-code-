a360e353341cf94fe6e32f205cb98a9e06f31d61
Anuj Kumar Pandey <83698322+codermal7@users.noreply.github.com>
2023-03-30 12:03:50 +0530

    Time: 9 ms (38.12%), Space: 13.7 MB (84.03%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -1,23 +1,3 @@
-// class Solution {
-// public:
-//     int minCostClimbingStairs(vector<int>& cost) {
-//         int a=0,b,l,r;
-//         int n=cost.size();
-//         for(int i=1;i<n;i++)
-//         {
-//             l=a+abs(cost[i]-cost[i-1]);
-//             r=INT_MAX;
-//             if(i>1)
-//             r=b+abs(cost[i]-cost[i-2]);
-//             b=a;
-//             a=min(l,r);
-//         }
-//         return a;
-//     }
-    
-// };
-
-
 class Solution {
 public:
     int minCostClimbingStairs(vector<int>& cost) {
