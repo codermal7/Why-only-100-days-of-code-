75acb6f436b0a767d362e2ad0d170232cc12866d
Anuj Kumar Pandey <83698322+codermal7@users.noreply.github.com>
2023-03-31 00:43:16 +0530

    Time: 24 ms (30.46%), Space: 15.6 MB (36.25%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,6 +1,5 @@
 class Solution {
-    void combination(vector<int>& candidates, int target, vector<int> currComb, int currSum, int currIndex, vector<vector<int>>& ans)
-    {
+    void combination(vector<int>& candidates, int target, vector<int> currComb, int currSum, int currIndex, vector<vector<int>>& ans){
         if(currSum>target) return; //backtrack
         if(currSum==target){
             ans.push_back(currComb); //store the solution and backtrack
