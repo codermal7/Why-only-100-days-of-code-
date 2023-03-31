9f2eba60f18fe5c7644a04a3df3d152f590d1f2a
Anuj Kumar Pandey <83698322+codermal7@users.noreply.github.com>
2023-03-31 22:50:02 +0530

    Time: 65 ms (76.43%), Space: 48.2 MB (94.92%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,12 +1,15 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        int l=nums.size()-1;
-        for(int i=nums.size()-1;i>=0;i--){
-            if(i+nums[i]>=l){
-                l=i;
-            }
+        int min_to_reach = nums.size() - 1, i;
+        
+        for(i = nums.size() - 2; i >=0; i--) {
+            if(nums[i] + i >= min_to_reach)
+                min_to_reach = i;
         }
-        return (l==0);
+        if(min_to_reach == 0)
+            return true;
+        else
+            return false;
     }
 };
