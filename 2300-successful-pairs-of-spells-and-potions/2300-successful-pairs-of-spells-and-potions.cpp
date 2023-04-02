810c7a93950783bb033837b8ee1a28686b35fd7e
Anuj Kumar Pandey <83698322+codermal7@users.noreply.github.com>
2023-04-02 11:29:36 +0530

    Time: 243 ms (75.28%), Space: 97.4 MB (67.55%) - LeetHub

--- 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.cpp
+++ 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.cpp
@@ -1,17 +1,27 @@
 class Solution {
 public:
-    vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
-        
-        sort(potions.begin(), potions.end());
-        vector<int> ans;
-        for(int i=0; i<spells.size(); i++)
-        {
-            long long int z = success / (long long int)spells[i];
-            if(spells[i] * z < success)
-                z++;
-            int y = lower_bound(potions.begin(), potions.end(), z) - potions.begin();
-            ans.push_back(potions.size() - y);
+    vector<int> successfulPairs(vector<int> &spells, vector<int> &potions, long long success) {
+        // suppport variables
+        int len = spells.size(), maxVal = *max_element(begin(spells), end(spells)) + 1, precomputed[maxVal];
+        long long threshold;
+        double spellVal;
+        vector<int> res(len);
+        // preparing precomputed
+        fill(precomputed, precomputed + maxVal, -1);
+        // preparing potions for the BS
+        sort(begin(potions), end(potions));
+        for (int i = 0, tmpSpell; i < len; i++) {
+            tmpSpell = spellVal = spells[i];
+            // checking if we already had computed a result for that value
+            if (precomputed[tmpSpell] != -1) {
+                res[i] = precomputed[tmpSpell];
+                continue;
+            }
+            // computing the threshold value we need to get in potions, if we did not see if before
+            threshold = (success + spellVal - 1) / spellVal;
+            // adding to res how many such values are in potions through BS
+            res[i] = precomputed[tmpSpell] = end(potions) - lower_bound(begin(potions), end(potions), threshold);
         }
-        return ans;
+        return res;
     }
 };
