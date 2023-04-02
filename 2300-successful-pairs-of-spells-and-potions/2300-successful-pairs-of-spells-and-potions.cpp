a48f143203ec1bb14001555d8a05bd2cc6228615
Anuj Kumar Pandey <83698322+codermal7@users.noreply.github.com>
2023-04-02 11:31:11 +0530

    Time: 240 ms (78.37%), Space: 97.7 MB (67.55%) - LeetHub

--- 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.cpp
+++ 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.cpp
@@ -1,45 +1,21 @@
-// class Solution {
-// public:
-//     vector<int> successfulPairs(vector<int> &spells, vector<int> &potions, long long success) {
-//         int len = spells.size(), maxVal = *max_element(begin(spells), end(spells)) + 1, precomputed[maxVal];
-//         long long threshold;
-//         double spellVal;
-//         vector<int> res(len);
-//         fill(precomputed, precomputed + maxVal, -1);
-//         sort(begin(potions), end(potions));
-//         for (int i = 0, tmpSpell; i < len; i++) {
-//             tmpSpell = spellVal = spells[i];
-//             if (precomputed[tmpSpell] != -1) {
-//                 res[i] = precomputed[tmpSpell];
-//                 continue;
-//             }
-//             threshold = (success + spellVal - 1) / spellVal;
-//             res[i] = precomputed[tmpSpell] = end(potions) - lower_bound(begin(potions), end(potions), threshold);
-//         }
-//         return res;
-//     }
-// };
 class Solution {
 public:
     vector<int> successfulPairs(vector<int> &spells, vector<int> &potions, long long success) {
-        int len = spells.size(), maxVal = *max_element(begin(spells), end(spells)) + 1;
+        int len = spells.size(), maxVal = *max_element(begin(spells), end(spells)) + 1, precomputed[maxVal];
         long long threshold;
         double spellVal;
         vector<int> res(len);
-        vector<int> precomputed(maxVal, -1);
+        fill(precomputed, precomputed + maxVal, -1);
         sort(begin(potions), end(potions));
-        
-        for (int i = 0; i < len; ++i) {
-            int tmpSpell = spells[i];
+        for (int i = 0, tmpSpell; i < len; i++) {
+            tmpSpell = spellVal = spells[i];
             if (precomputed[tmpSpell] != -1) {
                 res[i] = precomputed[tmpSpell];
-            } else {
-                spellVal = static_cast<double>(tmpSpell);
-                threshold = (success + spellVal - 1) / spellVal;
-                res[i] = precomputed[tmpSpell] = end(potions) - lower_bound(begin(potions), end(potions), threshold);
+                continue;
             }
+            threshold = (success + spellVal - 1) / spellVal;
+            res[i] = precomputed[tmpSpell] = end(potions) - lower_bound(begin(potions), end(potions), threshold);
         }
-        
         return res;
     }
 };
