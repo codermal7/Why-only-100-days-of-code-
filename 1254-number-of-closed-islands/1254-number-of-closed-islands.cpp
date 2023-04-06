9131c7b9a99c4cbe26bac8f9ed6f9fb722b981d0
Anuj Kumar Pandey <83698322+codermal7@users.noreply.github.com>
2023-04-06 11:17:38 +0530

    Time: 13 ms (71.49%), Space: 9.4 MB (77.35%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
@@ -1,26 +1,37 @@
 class Solution {
-    void util(vector<vector<int>>& grid, int i, int j) {
-        if(i<0 || j<0 || i>=grid.size() || j>=grid[0].size() || grid[i][j])
-            return;
-        grid[i][j] = 1;
-        util(grid,i-1,j);
-        util(grid,i,j-1);
-        util(grid,i+1,j);
-        util(grid,i,j+1);
-    }
 public:
-    int closedIsland(vector<vector<int>>& grid) {
-        int c = 0;
-        int n = grid.size();
-        int m = grid[0].size();
-        for(int i=0; i<n; i++)
-            util(grid,i,0), util(grid,i,m-1);
-        for(int j=0; j<m; j++)
-            util(grid,0,j), util(grid,n-1,j);
-        for(int i=0; i<n; i++)
-            for(int j=0; j<m; j++)
-                if(!grid[i][j])
-                    util(grid,i,j), c++;
+    
+    int f=0;
+    void helper(int i,int j,int n,int c,int oldc,vector<vector<int>>& grid)
+    {
+        if(i<0 || i>=n || j<0 || j>=c || grid[i][j]!=oldc)
+            return ;
+        if(i==0 || i==n-1 || j==0 || j==c-1)
+            f=1;
+        grid[i][j]=-1;
+        helper(i+1,j,n,c,oldc,grid);
+        helper(i-1,j,n,c,oldc,grid);
+        helper(i,j+1,n,c,oldc,grid);
+        helper(i,j-1,n,c,oldc,grid);
+    }
+    
+    int closedIsland(vector<vector<int>>& grid) 
+    {
+        
+        int c=0;
+        for(int i=0;i<grid.size();i++)
+        {
+            for(int j=0;j<grid[i].size();j++)
+            {
+                f=0;
+                if(grid[i][j]==0)
+                {
+                    helper(i,j,grid.size(),grid[i].size(),grid[i][j],grid);
+                    if(f==0)
+                        c+=1;
+                }
+            }
+        }
         return c;
     }
 };
