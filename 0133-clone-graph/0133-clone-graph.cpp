49de720eecc734a2ff3e8a72061472df3e3588fd
Anuj Kumar Pandey <83698322+codermal7@users.noreply.github.com>
2023-04-08 19:43:31 +0530

    Time: 11 ms (20.76%), Space: 8.8 MB (53.37%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.cpp
+++ 0133-clone-graph/0133-clone-graph.cpp
@@ -1,32 +1,23 @@
 class Solution {
 public:
     Node* cloneGraph(Node* node) {
-        if(node==NULL) return NULL;
-        Node* root = new Node(node->val);
-        vector<pair<int,Node*>>vis(101,{0,NULL});
-        vis[node->val]={1,root};
-        queue<pair<Node*,Node*>>q;
-        q.push({node,root});
-        while(!q.empty()){
-            Node* temp=q.front().first;
-            Node* r=q.front().second;
-            q.pop();
-            for(auto j:r->neighbors){
-                if(j->val==temp->val) {r=j; break;}
-            }
-            for(auto i:temp->neighbors){
-               
-                if(vis[i->val].first==0){
-                    Node* t=new Node(i->val);
-                    r->neighbors.push_back(t);
-                    vis[i->val]={1,t};
-                    q.push({i,r});
-                }
-                else{
-                    r->neighbors.push_back(vis[i->val].second);
-                }
-            }
+        unordered_map<Node*, Node*> oldtonew;
+        return dfs(node, oldtonew);
+    }
+
+    Node* dfs(Node* node, unordered_map<Node*, Node*>& oldtonew) {
+        if (!node) {
+            return nullptr;
         }
-        return root;
+        if (oldtonew.count(node)) {
+            return oldtonew[node];
+        }
+        Node* copy = new Node(node->val);
+        oldtonew[node] = copy;
+        for (Node* n : node->neighbors) {
+            copy->neighbors.push_back(dfs(n, oldtonew));
+        }
+        return copy;
+        
     }
 };
