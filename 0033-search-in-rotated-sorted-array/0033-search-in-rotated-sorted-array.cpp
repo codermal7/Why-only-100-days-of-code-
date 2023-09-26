f6f4035bfd8ee345226ce2457c3dbbfd5f49cdfb
Anuj Kumar Pandey <83698322+codermal7@users.noreply.github.com>
2023-09-26 13:17:33 +0530

    Time: 3 ms (64.99%), Space: 11.4 MB (7.04%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,32 +1,28 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int l = 0, r = nums.size()-1;
-        while (l <= r) {
-            int mid = (l+r) / 2;
-            if (target == nums[mid])
-                return mid;
-            // there exists rotation; the middle element is in the left part of the array
-            if (nums[mid] > nums[r]) {
-                if (target < nums[mid] && target >= nums[l])
-                    r = mid - 1;
+        int s=0,e=nums.size()-1,mid;
+        while(s<=e)
+        {
+            mid=s+((e-s)/2);
+            if(target==nums[mid])
+            return mid;
+            //SORTED LEFT HALF
+            else if(nums[s]<=nums[mid])
+            {
+                if(nums[s]<=target && target<=nums[mid])
+                e=mid-1;
                 else
-                    l = mid + 1;
+                s=mid+1;
             }
-            // there exists rotation; the middle element is in the right part of the array
-            else if (nums[mid] < nums[l]) {
-                if (target > nums[mid] && target <= nums[r])
-                    l = mid + 1;
+            //SORTED RIGHT HALF
+            else{
+                if(nums[mid]<=target && target<=nums[e])
+                s=mid+1;
                 else
-                    r = mid - 1;
+                e=mid-1;
             }
-            // there is no rotation; just like normal binary search
-            else {
-                if (target < nums[mid])
-                    r = mid - 1;
-                else
-                    l = mid + 1;
-            }
+
         }
         return -1;
     }
