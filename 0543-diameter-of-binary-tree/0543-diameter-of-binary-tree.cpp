9c25945d2e9460fa5420bfeea629735eac226649
Anuj Kumar Pandey <83698322+codermal7@users.noreply.github.com>
2023-10-20 11:46:55 +0530

    Time: 11 ms (33.12%), Space: 20.4 MB (64.42%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
@@ -11,6 +11,7 @@
  */
 class Solution {
 public:
+    // O(n) code
     int diameterOfBinaryTree(TreeNode* root) {
         int diam = 0;
         height(root,diam);
@@ -25,4 +26,22 @@ public:
 
         return 1+max(lh,rh);
     }
+
+
+    // O(n^2) Code
+    // int diameterOfBinaryTree(TreeNode* root) {
+    //     int diam = 0;
+    //     height(root, diam);
+    //     return diam;
+    // }
+    // int height(TreeNode* root, int& diam){
+    //     if(root==NULL)
+    //     return 0;
+    //     int lh=height(root->left, diam);
+    //     int rh=height(root->right, diam);
+    //     diam = max(diam,lh+rh);
+    //     height(root->left, diam);
+    //     height(root->right, diam);
+    //     return 1+max(lh,rh);
+    // }
 };
