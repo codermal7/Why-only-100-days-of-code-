123cfc31f691478dbc4423b853e1d1a15428cc97
Anuj Kumar Pandey <83698322+codermal7@users.noreply.github.com>
2024-01-22 20:58:11 +0530

    Time: 49 ms (33.22%), Space: 34.4 MB (8.31%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.cpp
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -1,35 +1,63 @@
-#include <bits/stdc++.h>
+// #include <bits/stdc++.h>
+// class Solution {
+// public:
+//     vector<int> findErrorNums(vector<int>& nums) {
+//         int n=nums.size(),c=1;vector<int> ans;vector<int> miss;
+//         sort(nums.begin(),nums.end());bool check;
+//         while(c<=n)
+//         {
+//             check=false;
+//             for(int i=0;i<n;i++)
+//             {
+//                 if(c==nums[i])
+//                 {
+//                     check=true;
+//                     break;
+//                 }
+//             }
+//             if(check==false)
+//             miss.push_back(c);
+//             c++;
+//         }
+//         for(int i=0;i<n;i++)
+//         {
+//             for(int j=i+1;j<n;j++)
+//             {
+//                 if(nums[i]==nums[j])
+//                 {
+//                     ans.push_back(nums[i]);
+//                 }
+//             }
+//         }
+//         ans.insert(ans.end(), miss.begin(), miss.end());
+//         return ans;
+//     }
+// };
+
+
 class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
-        int n=nums.size(),c=1;vector<int> ans;vector<int> miss;
-        sort(nums.begin(),nums.end());bool check;
-        while(c<=n)
-        {
-            check=false;
-            for(int i=0;i<n;i++)
-            {
-                if(c==nums[i])
-                {
-                    check=true;
-                    break;
-                }
-            }
-            if(check==false)
-            miss.push_back(c);
-            c++;
-        }
-        for(int i=0;i<n;i++)
-        {
-            for(int j=i+1;j<n;j++)
-            {
-                if(nums[i]==nums[j])
-                {
-                    ans.push_back(nums[i]);
-                }
+        int n = nums.size();
+        unordered_set<int> numSet;
+        int duplicate = -1, missing = -1;
+
+        for (int num : nums) {
+            if (numSet.find(num) != numSet.end()) {
+                duplicate = num;
+            } else {
+                numSet.insert(num);
             }
         }
-        ans.insert(ans.end(), miss.begin(), miss.end());
-        return ans;
+
+        for (int i = 1; i <= n; ++i) {
+            if (numSet.find(i) == numSet.end()) {
+                missing = i;
+                break;
+            }
+        }
+        return {duplicate, missing};
     }
 };
+
+
