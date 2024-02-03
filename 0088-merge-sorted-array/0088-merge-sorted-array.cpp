94664012bc8a69384289583a80fb296fe5cc0917
Anuj Kumar Pandey <83698322+codermal7@users.noreply.github.com>
2024-02-03 23:24:28 +0530

    Time: 3 ms (39.48%), Space: 10.9 MB (17.67%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,11 +1,14 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        // BETTER 
         // for (int j = 0, i = m; j<n; j++){
         //     nums1[i] = nums2[j];
         //     i++;
         // }
         // sort(nums1.begin(),nums1.end());
+
+        // OPTIMAL
         int i=m-1,j=n-1,k=m+n-1;
         while(j>=0)
         {
