585a93d736dc2876d0efe6e748f99797750552f9
Anuj Kumar Pandey <83698322+codermal7@users.noreply.github.com>
2024-02-03 12:39:21 +0530

    Time: 90 ms (73.4%), Space: 95.7 MB (21.02%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,27 +1,16 @@
 class Solution {
 public:
-    int find(vector<int> &prices, int i,int k,bool buy,vector<vector<int>> &v)
-    {
-        //if we have no stock or we have no chance of transaction(k=0)
-        if(i>=prices.size()||k<=0) return 0;
-        if(v[i][buy]!=-1) return v[i][buy];
-        
-        if(buy)  //if we are buying then next time we will sell else next time we will buy
-        {        //-prices[i], because bought stock of prices[i], expend money
-            
-           return v[i][buy]=max(-prices[i]+find(prices,i+1,k,!buy,v),find(prices,i+1,k,buy,v)); 
-        }
-        else    //it's time to sell , now decrease k, we have done 1 transaction
-        {       //+prices[i], because we now gain (i.e) sell our stock at rate of prices[i]
-           return v[i][buy]=max( prices[i]+find(prices,i+1,k-1,!buy,v),find(prices,i+1,k,buy,v)); 
-        }
-    }
     int maxProfit(vector<int>& prices) {
-        
-        int n=prices.size(); 
-        vector<vector<int>> v(n,vector<int> (2,-1));
-        //passing here buy=1 because we will first buy then sell
-        //we can do atmost k=1 transaction
-        return find(prices,0,1,1,v);
+        int n=prices.size();
+        int mini=INT_MAX, maxi=0;
+        for(int i=0;i<n;i++)
+        {
+            if(prices[i]<mini)
+            mini=prices[i];
+            else{
+                maxi=max(prices[i]-mini,maxi);
+            }
+        }
+        return maxi;
     }
 };
