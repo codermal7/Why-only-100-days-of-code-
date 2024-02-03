60f0b209daa217778fcddfd3f85447f9b7cbfa92
Anuj Kumar Pandey <83698322+codermal7@users.noreply.github.com>
2024-02-04 00:09:35 +0530

    Time: 66 ms (96.6%), Space: 63.5 MB (37.49%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -1,6 +1,6 @@
 class Solution {
-public:
-    int findDuplicate(vector<int>& nums) {
+    public: int findDuplicate(vector < int > & nums) {
+        // TC = O(n), SC = O(n);
         // unordered_map<int,int> mp;
         // for (int i = 0; i < nums.size(); i++) 
         // {
@@ -12,14 +12,30 @@ public:
         //     return it.first;
         // }
         // return -1;
-        sort(nums.begin(),nums.end());
-        int n = nums.size();
-        for (int i = 0; i < n - 1; i++) {
-            if(nums[i] == nums[i + 1]) 
-            {
-                return nums[i];
-            }
+
+        // TC = O(nlog(n)), SC = O(1);
+        // sort(nums.begin(),nums.end());
+        // int n = nums.size();
+        // for (int i = 0; i < n - 1; i++) {
+        //     if(nums[i] == nums[i + 1]) 
+        //     {
+        //         return nums[i];
+        //     }
+        // }
+        // return -1;
+
+        // TC = O(n), SC = O(1);
+        int slow = nums[0];
+        int fast = nums[0];
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+        fast = nums[0];
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
         }
-        return -1;
+        return slow;
     }
 };
