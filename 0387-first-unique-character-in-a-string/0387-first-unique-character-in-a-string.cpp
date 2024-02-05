92e6da01f717e8e69233d55e44555ceae4e8c5ba
Anuj Kumar Pandey <83698322+codermal7@users.noreply.github.com>
2024-02-05 17:29:37 +0530

    Time: 43 ms (26.45%), Space: 14.6 MB (10.77%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
@@ -13,19 +13,33 @@ public:
         // }
         // return -1;
 
-        int n=s.length();
-        for (int i = 0; i < n; i++) {
-            bool repeated = false;
-            for (int j = 0; j < n; j++) {
-                if (i != j && s[i] == s[j]) {
-                    repeated = true;
-                    break;
-                }
-            }
-            if (!repeated) {
-                return i;
+        // int n=s.length();
+        // for (int i = 0; i < n; i++) {
+        //     bool repeated = false;
+        //     for (int j = 0; j < n; j++) {
+        //         if (i != j && s[i] == s[j]) {
+        //             repeated = true;
+        //             break;
+        //         }
+        //     }
+        //     if (!repeated) {
+        //         return i;
+        //     }
+        // }
+        // return -1;
+
+        unordered_map<char, int> count;
+        queue<char> q;
+
+        for (char c : s) {
+            count[c]++;
+            q.push(c);
+
+            while (!q.empty() && count[q.front()] > 1) {
+                q.pop();
             }
         }
-        return -1;
+
+        return q.empty() ? -1 : find(s.begin(), s.end(), q.front()) - s.begin();
     }
 };
