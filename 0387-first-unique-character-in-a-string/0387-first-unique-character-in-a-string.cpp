092f0983766a29131bc394fa4ee13c9986347b27
Anuj Kumar Pandey <83698322+codermal7@users.noreply.github.com>
2024-02-05 17:32:40 +0530

    Time: 34 ms (51.74%), Space: 13.3 MB (10.77%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     int firstUniqChar(string s) {
-        // unordered_map<char,int> mp;
-        // for(char ch:s)
-        // {
-        //     mp[ch]++;
-        // }
-        // for (int i = 0; i < s.length(); i++) {
-        //     if (mp[s[i]] == 1) {
-        //         return i;
-        //     }
-        // }
-        // return -1;
+        unordered_map<char,int> mp;
+        for(char ch:s)
+        {
+            mp[ch]++;
+        }
+        for (int i = 0; i < s.length(); i++) {
+            if (mp[s[i]] == 1) {
+                return i;
+            }
+        }
+        return -1;
 
         // int n=s.length();
         // for (int i = 0; i < n; i++) {
@@ -28,23 +28,23 @@ public:
         // }
         // return -1;
 
-        std::unordered_map<char, int> frequencyMap;
-        std::queue<std::pair<char, int>> charQueue;
+        // std::unordered_map<char, int> frequencyMap;
+        // std::queue<std::pair<char, int>> charQueue;
 
-        for (int i = 0; i < s.size(); i++) {
-            frequencyMap[s[i]]++;
-            charQueue.push(std::make_pair(s[i], i));
-        }
+        // for (int i = 0; i < s.size(); i++) {
+        //     frequencyMap[s[i]]++;
+        //     charQueue.push(std::make_pair(s[i], i));
+        // }
 
-        while (!charQueue.empty()) {
-            char currentChar = charQueue.front().first;
-            int currentIndex = charQueue.front().second;
-            if (frequencyMap[currentChar] == 1) {
-                return currentIndex;
-            } else {
-                charQueue.pop();
-            }
-        }
-        return -1;
+        // while (!charQueue.empty()) {
+        //     char currentChar = charQueue.front().first;
+        //     int currentIndex = charQueue.front().second;
+        //     if (frequencyMap[currentChar] == 1) {
+        //         return currentIndex;
+        //     } else {
+        //         charQueue.pop();
+        //     }
+        // }
+        // return -1;
     }
 };
