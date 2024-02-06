50b2bc24ddf9958e629654c90a03c66826f85a49
Anuj Kumar Pandey <83698322+codermal7@users.noreply.github.com>
2024-02-06 21:27:18 +0530

    Time: 32 ms (50.57%), Space: 25.6 MB (13.73%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,50 +1,50 @@
 class Solution {
 public:
-    // string generateWord(string& word)
-    // {
-    //     int arr[26]={0};
-    //     for(char ch:word)
-    //     {
-    //         arr[ch-'a']++;
-    //     }
-    //     string new_word="";
-    //     for(int i=0;i<26;i++)
-    //     {
-    //         int freq=arr[i];
-    //         if(freq>0)
-    //         new_word+=string(freq,i+'a');
-    //     }
-    //     return new_word;
-    // }
+    string generateWord(string& word)
+    {
+        int arr[26]={0};
+        for(char ch:word)
+        {
+            arr[ch-'a']++;
+        }
+        string new_word="";
+        for(int i=0;i<26;i++)
+        {
+            int freq=arr[i];
+            if(freq>0)
+            new_word+=string(freq,i+'a');
+        }
+        return new_word;
+    }
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         if(strs.size()==0)
         return {{}};
-        vector<vector<string>> ans;
-        unordered_map<string,vector<string>> mp;
-        for(int i=0;i<strs.size();i++)
-        {
-            string temp=strs[i];
-            sort(temp.begin(),temp.end());
-            mp[temp].push_back(strs[i]);
-        }
-        for(auto it:mp)
-        {
-            ans.push_back(it.second);
-        }
-        return ans;
-
         // vector<vector<string>> ans;
         // unordered_map<string,vector<string>> mp;
         // for(int i=0;i<strs.size();i++)
         // {
-        //     string word=strs[i];
-        //     string new_word=generateWord(word);
-        //     mp[new_word].push_back(word);
+        //     string temp=strs[i];
+        //     sort(temp.begin(),temp.end());
+        //     mp[temp].push_back(strs[i]);
         // }
         // for(auto it:mp)
         // {
         //     ans.push_back(it.second);
         // }
         // return ans;
+
+        vector<vector<string>> ans;
+        unordered_map<string,vector<string>> mp;
+        for(int i=0;i<strs.size();i++)
+        {
+            string word=strs[i];
+            string new_word=generateWord(word);
+            mp[new_word].push_back(word);
+        }
+        for(auto it:mp)
+        {
+            ans.push_back(it.second);
+        }
+        return ans;
     }
 };
