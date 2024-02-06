e7b8fe31e549a5766e551d0c9c18244a47749766
Anuj Kumar Pandey <83698322+codermal7@users.noreply.github.com>
2024-02-06 21:25:14 +0530

    Time: 37 ms (32.73%), Space: 25.5 MB (13.73%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -17,20 +17,6 @@ public:
         return new_word;
     }
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        // vector<vector<string>> ans;
-        // unordered_map<string,vector<string>> mp;
-        // for(int i=0;i<strs.size();i++)
-        // {
-        //     string temp=strs[i];
-        //     sort(temp.begin(),temp.end());
-        //     mp[temp].push_back(strs[i]);
-        // }
-        // for(auto it:mp)
-        // {
-        //     ans.push_back(it.second);
-        // }
-        // return ans;
-
         vector<vector<string>> ans;
         unordered_map<string,vector<string>> mp;
         for(int i=0;i<strs.size();i++)
