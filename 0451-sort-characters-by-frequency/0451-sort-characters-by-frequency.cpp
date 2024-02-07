915a2c02d5c9f60faab8604d7db4b634f5525e94
Anuj Kumar Pandey <83698322+codermal7@users.noreply.github.com>
2024-02-07 23:50:49 +0530

    Time: 10 ms (69.23%), Space: 10 MB (35.93%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -1,21 +1,39 @@
 class Solution {
 public:
+    typedef pair<char,int> p;
     string frequencySort(string s) {
-        unordered_map<char,int> mp;
-        multimap<int,char> r;
-        string ss="";
+        // map<char,int> mp;
+        // multimap<int,char> r;
+        // string ss="";
 
-        for(auto a : s)
-            mp[a]++;
+        // for(auto a : s)
+        //     mp[a]++;
 
-        for(auto a : mp)
-            r.insert({a.second, a.first});
+        // for(auto a : mp)
+        //     r.insert({a.second, a.first});
 
-        for(auto it = r.rbegin(); it != r.rend(); ++it){
-            ss += string(it->first, it->second);
-        } 
+        // for(auto it = r.rbegin(); it != r.rend(); ++it){
+        //     ss += string(it->first, it->second);
+        // }
 
-        return ss;
+        // return ss;
+        string res="";
+        vector<p> vec(123);
+        for(char ch:s)
+        {
+            vec[ch].first = ch;
+            vec[ch].second++;
+        }
+        
+        auto comp = [&](p &a, p &b) {
+            return a.second > b.second;
+        };
+        sort(vec.begin(),vec.end(),comp);
+        for(auto it:vec)
+        {
+            if(it.second>0)
+            res+=string(it.second,it.first);
+        }
+        return res;
     }
 };
-
