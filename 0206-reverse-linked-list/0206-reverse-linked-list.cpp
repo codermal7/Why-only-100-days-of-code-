7bdcfdd14673f025168cab3a090fa0b284d1a1f7
Anuj Kumar Pandey <83698322+codermal7@users.noreply.github.com>
2024-02-09 20:58:18 +0530

    Time: 5 ms (36.31%), Space: 12 MB (5.71%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -23,20 +23,31 @@
 //     }
 // };
 
+// class Solution {
+// public:
+//     ListNode* reverseList(ListNode* head) {
+
+//         ListNode* prev = NULL;
+//         ListNode* curr = head;
+
+//         while(curr != NULL){
+//             ListNode* forward = curr->next;
+//             curr->next = prev;
+//             prev = curr;
+//             curr = forward;        
+//         }
+//         return prev;
+//     }
+// };
+
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
-
+        if(head == NULL || head->next == NULL) return head;
         ListNode* prev = NULL;
-        ListNode* curr = head;
-
-        while(curr != NULL){
-            ListNode* forward = curr->next;
-            curr->next = prev;
-            prev = curr;
-            curr = forward;
-            
-        }
-        return prev;
+        ListNode* h2 = reverseList(head->next);
+        head->next->next = head;
+        head->next=prev;
+        return h2;
     }
 };
