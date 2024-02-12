761157e214dd2effec9f9766317a66dcc2dfbee7
Anuj Kumar Pandey <83698322+codermal7@users.noreply.github.com>
2024-02-12 16:59:45 +0530

    Time: 32 ms (10.08%), Space: 27.8 MB (5.28%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,20 +1,31 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        int n=nums.size(), c=0;
-        sort(nums.begin(),nums.end());
-        for(int i=0;i<n;i++)
-        {
-            if(nums[i]==c)
-            {
-                c++;
-                continue;
-            }
-            else
-            {
-                return c;
+        // int n=nums.size(), c=0;
+        // sort(nums.begin(),nums.end());
+        // for(int i=0;i<n;i++)
+        // {
+        //     if(nums[i]==c)
+        //     {
+        //         c++;
+        //         continue;
+        //     }
+        //     else
+        //     {
+        //         return c;
+        //     }
+        // }
+        // return c;
+        unordered_map<int, bool> mp;
+        for (int num : nums) {
+            mp[num] = true;
+        }
+        int n = nums.size();
+        for (int i = 0; i <= n; i++) {
+            if (mp.find(i) == mp.end()) {
+                return i;
             }
         }
-        return c;
+        return n;
     }
 };
