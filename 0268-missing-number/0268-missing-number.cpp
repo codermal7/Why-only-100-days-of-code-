86059a14590a3a7d1e63cd95d35c48ce8eeee3cc
Anuj Kumar Pandey <83698322+codermal7@users.noreply.github.com>
2024-02-12 17:02:05 +0530

    Time: 16 ms (47.95%), Space: 20.3 MB (38.01%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -16,16 +16,23 @@ public:
         //     }
         // }
         // return c;
-        unordered_map<int, bool> mp;
-        for (int num : nums) {
-            mp[num] = true;
+        // unordered_map<int, bool> mp;
+        // for (int num : nums) {
+        //     mp[num] = true;
+        // }
+        // int n = nums.size();
+        // for (int i = 0; i <= n; i++) {
+        //     if (mp.find(i) == mp.end()) {
+        //         return i;
+        //     }
+        // }
+        // return n;
+        int sum=0,n=nums.size() ;
+        int range_sum= n*(n+1)/2;
+        for(int i=0;i<n;i++)
+        {
+            sum+=nums[i];
         }
-        int n = nums.size();
-        for (int i = 0; i <= n; i++) {
-            if (mp.find(i) == mp.end()) {
-                return i;
-            }
-        }
-        return n;
+        return (range_sum-sum);
     }
 };
