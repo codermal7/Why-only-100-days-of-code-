6f67a16fcbcbb39014e9a1b7a410ead148efa4f2
Anuj Kumar Pandey <83698322+codermal7@users.noreply.github.com>
2024-02-20 15:54:17 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,30 +1,30 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char> st;
-         for (char ch: s) {
-             if (ch == '(' || ch == '{' || ch == '[')
-                 st.push(ch);
-             if (ch == '}' ) {
-                 if ( !st.empty() && st.top() == '{')
-                     st.pop();
-                 else
-                     return false;
-             }
-             else if (ch == ')' ) {
-                 if ( !st.empty() && st.top() == '(')
-                     st.pop();
-                 else
-                     return false;
-             }
-             else if (ch == ']' ) {
-                 if ( !st.empty() && st.top() == '[')
-                     st.pop();
-                 else
-                     return false;
-             }
-         }
-        return st.empty();
-        
+        if(s[0]==')' || s[0]==']' || s[0]=='}')
+        return false;
+        stack<char> st; int i=1;
+        st.push(s[0]);
+        while(!st.empty())
+        {
+            if((s[i]=='(' || s[i]=='{' || s[i]=='[') && (i<s.size()))
+            {
+                st.push(s[0]);
+            }
+            else if(s[i]==')' || s[i]==']' || s[i]=='}')
+            {
+                char ch=st.top();
+                if((ch=='(' && s[i]==')') || (ch=='[' && s[i]==']') || 
+                (ch=='{' && s[i]=='}'))
+                st.pop();
+                else
+                return false;
+            }
+            else{
+                return false;
+            }
+            i++;
+        }
+        return true;
     }
 };
