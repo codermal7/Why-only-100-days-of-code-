a330918587c974533ef5c97ea3b6876a26b3ed0e
Anuj Kumar Pandey <83698322+codermal7@users.noreply.github.com>
2024-02-20 15:07:04 +0530

    Time: 24 ms (15.61%), Space: 21 MB (10.18%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -34,17 +34,31 @@ public:
         //     sum+=nums[i];
         // }
         // return (range_sum-sum);
-        int sum1=0;
-        int sum2=0;
+        // int sum1=0;
+        // int sum2=0;
 
-        for(int i=0; i<=nums.size(); i++){
-            sum1=sum1+i;
+        // for(int i=0; i<=nums.size(); i++){
+        //     sum1=sum1+i;
+        // }
+
+        // for(int i=0; i<nums.size(); i++){
+        //     sum2=sum2+nums[i];
+        // }
+
+        // return sum1-sum2;
+
+        int n=nums.size(), c=0;
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i]==c)
+                c++;
+            else
+            break;
         }
+        return c;
 
-        for(int i=0; i<nums.size(); i++){
-            sum2=sum2+nums[i];
-        }
 
-        return sum1-sum2;
+
     }
 };
