0113d3e2b44ab87af23944a4a4173ed3fbddfa3e
Anuj Kumar Pandey <83698322+codermal7@users.noreply.github.com>
2024-02-22 13:31:50 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1039-find-the-town-judge/1039-find-the-town-judge.cpp
+++ 1039-find-the-town-judge/1039-find-the-town-judge.cpp
@@ -59,21 +59,44 @@
 //     return 'c';
 // }();
 
+// #pragma GCC optimize("O3", "unroll-loops")
+// class Solution {
+// public:
+//     int findJudge(int n, vector<vector<int>>& trust) {
+//         int trusting[1001]={0}, trusted[1001]={0};//1 <= n <= 1000
+//         for (vector<int>& e: trust){
+//             trusting[e[0]]++; //outdeg
+//             trusted[e[1]]++; // indeg
+//         }
+//         for(int i=1; i<=n; i++)
+//             if (trusting[i]==0 && trusted[i]==n-1) return i;
+//         return -1;      
+//     }
+// };
+// auto init = []() {
+//     ios::sync_with_stdio(0);
+//     cin.tie(0);
+//     cout.tie(0);
+//     return 'c';
+// }();
+
+
 #pragma GCC optimize("O3", "unroll-loops")
 class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& trust) {
-        int trusting[1001]={0}, trusted[1001]={0};//1 <= n <= 1000
-        for (vector<int>& e: trust){
-            trusting[e[0]]++; //outdeg
-            trusted[e[1]]++; // indeg
+        vector<int> ansvect(n + 1, 0); 
+        for (auto it : trust) {
+            ansvect[it[0]]--; 
+            ansvect[it[1]]++;
         }
-        for(int i=1; i<=n; i++)
-            if (trusting[i]==0 && trusted[i]==n-1) return i;
-        return -1;      
+        for (int i = 1; i <= n; ++i) {
+            if (ansvect[i] == n - 1)
+                return i;
+        }
+        return -1;
     }
-};
-
+}
 
 auto init = []() {
     ios::sync_with_stdio(0);
