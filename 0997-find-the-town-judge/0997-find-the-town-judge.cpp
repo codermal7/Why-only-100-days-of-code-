326abfbeeb999ef61845f4ec9a0504c1ced1a6f1
Anuj Kumar Pandey <83698322+codermal7@users.noreply.github.com>
2024-02-22 13:18:07 +0530

    Time: 159 ms (32.24%), Space: 73.1 MB (16.29%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.cpp
+++ 0997-find-the-town-judge/0997-find-the-town-judge.cpp
@@ -6,13 +6,16 @@ public:
     int findJudge(int n, vector<vector<int>>& trust) {
         if(trust.size()==1)
         return trust[0][1];
-        vector<int> ansvect(n + 1, 0); 
+        // vector<int> ansvect(n + 1, 0); 
+        unordered_map<int,int>mp;
         for (auto it : trust) {
-            ansvect[it[0]]--; 
-            ansvect[it[1]]++;
+            // ansvect[it[0]]--; 
+            // ansvect[it[1]]++;
+            mp[it[0]]--;
+            mp[it[1]]++;
         }
         for (int i = 1; i <= n; ++i) {
-            if (ansvect[i] == n - 1)
+            if (mp[i] == n - 1)
                 return i;
         }
         return -1;
