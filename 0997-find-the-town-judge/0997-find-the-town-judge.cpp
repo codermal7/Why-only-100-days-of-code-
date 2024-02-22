f77d6acb98d4772dcfa0eb54c8972106580ab900
Anuj Kumar Pandey <83698322+codermal7@users.noreply.github.com>
2024-02-22 13:35:42 +0530

    Time: 136 ms (64.31%), Space: 72.6 MB (17.7%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.cpp
+++ 0997-find-the-town-judge/0997-find-the-town-judge.cpp
@@ -13,11 +13,77 @@
 //         return -1;
 //     }
 // };
+// class Solution {
+// public:
+//     int findJudge(int n, vector<vector<int>>& trust) {
+//         vector<int> ansvect(n + 1, 0); 
+//         for (auto it : trust) {
+//             ansvect[it[0]]--; 
+//             ansvect[it[1]]++;
+//         }
+//         for (int i = 1; i <= n; ++i) {
+//             if (ansvect[i] == n - 1)
+//                 return i;
+//         }
+//         return -1;
+//     }
+// }
+
+// #pragma GCC optimize("O3", "unroll-loops")
+// class Solution {
+// public:
+//     using int2=pair<int, int>;
+//     int findJudge(int n, vector<vector<int>>& trust) {
+//         vector<int2> degArr(n);
+//         for(int i=0; i<n; i++)
+//             degArr[i]={i+1, 0};
+//         //Initialize once all, not 1 by 1
+//         unordered_map<int, int> deg(degArr.begin(), degArr.end());
+//         for (auto& e: trust){
+//             int a=e[0], b=e[1];
+//             if (deg.count(a)) deg.erase(a);//outdeg(a)>0
+//             if (deg.count(b)) deg[b]++; //Only consider outdeg(b)=0
+//         }
+//         for (auto& [i, d] : deg)
+//             if (d==n-1) return i;
+        
+//         return -1;      
+//     }
+// };
+
+
+// auto init = []() {
+//     ios::sync_with_stdio(0);
+//     cin.tie(0);
+//     cout.tie(0);
+//     return 'c';
+// }();
+
+// #pragma GCC optimize("O3", "unroll-loops")
+// class Solution {
+// public:
+//     int findJudge(int n, vector<vector<int>>& trust) {
+//         int trusting[1001]={0}, trusted[1001]={0};//1 <= n <= 1000
+//         for (vector<int>& e: trust){
+//             trusting[e[0]]++; //outdeg
+//             trusted[e[1]]++; // indeg
+//         }
+//         for(int i=1; i<=n; i++)
+//             if (trusting[i]==0 && trusted[i]==n-1) return i;
+//         return -1;      
+//     }
+// };
+// auto init = []() {
+//     ios::sync_with_stdio(0);
+//     cin.tie(0);
+//     cout.tie(0);
+//     return 'c';
+// }();
+
+#pragma GCC optimize("O3", "unroll-loops")
 class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& trust) {
-        if(trust.size()==1)
-        return trust[0][1];
         vector<int> ansvect(n + 1, 0); 
         for (auto it : trust) {
             ansvect[it[0]]--; 
@@ -30,3 +96,10 @@ public:
         return -1;
     }
 };
+
+auto init = []() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
