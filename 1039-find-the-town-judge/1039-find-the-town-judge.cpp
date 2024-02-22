d1cd598a19e85e1d93b914ab1164571b9da6ef95
Anuj Kumar Pandey <83698322+codermal7@users.noreply.github.com>
2024-02-22 13:26:03 +0530

    Time: 156 ms (35.52%), Space: 72.5 MB (17.7%) - LeetHub

--- 1039-find-the-town-judge/1039-find-the-town-judge.cpp
+++ 1039-find-the-town-judge/1039-find-the-town-judge.cpp
@@ -13,18 +13,38 @@
 //         return -1;
 //     }
 // };
+// class Solution {
+// public:
+//     int findJudge(int n, vector<vector<int>>& trust) {
+//         vector<int> ansvect(n + 1, 0); 
+//         for (auto it : trust) {
+//             ansvect[it[0]]--; 
+//             ansvect[it[1]]++;
+//         }
+//         for (int i = 1; i <= n; ++i) {
+//             if (ansvect[i] == n - 1)
+//                 return i;
+//         }
+//         return -1;
+//     }
+// };
+
 class Solution {
 public:
-    int findJudge(int n, vector<vector<int>>& trust) {
-        vector<int> ansvect(n + 1, 0); 
-        for (auto it : trust) {
-            ansvect[it[0]]--; 
-            ansvect[it[1]]++;
+    int findJudge(int n, vector<vector<int>>& trust) {  
+	 // Arrays to track trust given and received
+        vector<int> trusting(n + 1, 0);   
+        vector<int> trusted(n + 1, 0);     
+        for (auto i : trust) {                    // Count trust relationships
+            trusting[i[0]]++;                    // Increment trust given
+            trusted[i[1]]++;                    // Increment trust received
         }
-        for (int i = 1; i <= n; ++i) {
-            if (ansvect[i] == n - 1)
-                return i;
-        }
-        return -1;
+        int ans = -1;                            // Initialize result
+                       
+        for (int i = 1; i <= n; i++)           // Find the person trusted by everyone else
+            if (trusting[i] == 0 && trusted[i] == n - 1){ 
+                ans = i;             // Update result
+            }
+        return ans;                // Return judge's index or -1 if not found
     }
 };
