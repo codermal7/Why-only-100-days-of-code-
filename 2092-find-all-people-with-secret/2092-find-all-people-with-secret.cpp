91f1e278d84773cfc32d7e08962ac385949aedb8
Anuj Kumar Pandey <83698322+codermal7@users.noreply.github.com>
2024-02-24 15:31:37 +0530

    Time: 316 ms (99.59%), Space: 136.4 MB (95.49%) - LeetHub

--- 2092-find-all-people-with-secret/2092-find-all-people-with-secret.cpp
+++ 2092-find-all-people-with-secret/2092-find-all-people-with-secret.cpp
@@ -1,56 +1,54 @@
+#pragma GCC optimize("O3", "unroll-loops")
 class Solution {
 public:
-    vector<int> findAllPeople(int n, vector<vector<int>>& meetings, 
-    int firstPerson) 
-    {
-        set<int> ans = {0, firstPerson};
-        
-        vector<vector<pair<int, int>>> sortedMeetings;
-        sort(meetings.begin(), meetings.end(), 
-        [](const vector<int>& a, const vector<int>& b) 
-        {
-            return a[2] < b[2];
+#define MAX_N 100000
+    int find(const int* groups, int index){
+        while(index != groups[index])index = groups[index];
+        return index;
+    }
+
+    vector<int> findAllPeople(int n, vector<vector<int>>& meetings, int firstPerson) {
+        static int groups[MAX_N];
+        static vector<int> temp;
+
+        for (int i = 0; i < n; ++i) groups[i] = i;
+        groups[firstPerson] = 0;
+
+        sort(meetings.begin(), meetings.end(), [](const auto& l, const auto& r) {
+            return l[2] < r[2];
         });
 
-        set<int> seenTime;
-        
-        for (const auto& meeting : meetings) {
-            if (seenTime.find(meeting[2]) == seenTime.end()) {
-                seenTime.insert(meeting[2]);
-                sortedMeetings.push_back({});
+        const int SIZE = meetings.size();
+        int i = 0;
+        while (i < SIZE) {
+            int currentTime = meetings[i][2];
+            temp.clear();
+            while (i < SIZE && meetings[i][2] == currentTime) {
+                int g1 = find(groups, meetings[i][0]);
+                int g2 = find(groups, meetings[i][1]);
+                groups[max(g1, g2)] = min(g1, g2);
+                temp.push_back(meetings[i][0]);
+                temp.push_back(meetings[i][1]);
+                ++i;
             }
-            sortedMeetings.back().push_back({meeting[0], meeting[1]});
-        }
-
-        for (const auto& meetingGroup : sortedMeetings) {
-            set<int> peopleKnowSecret;
-            unordered_map<int, vector<int>> graph;
-            
-            for (const auto& pair : meetingGroup) {
-                graph[pair.first].push_back(pair.second);
-                graph[pair.second].push_back(pair.first);
-                
-                if(ans.find(pair.first) != ans.end()) 
-                peopleKnowSecret.insert(pair.first);
-                if(ans.find(pair.second)!=ans.end())                    
-                peopleKnowSecret.insert(pair.second);
-            }
-            
-            queue<int> queue;
-            for (int person : peopleKnowSecret) queue.push(person);
-        
-            while (!queue.empty()) {
-                int curr = queue.front();
-                queue.pop();
-                ans.insert(curr);
-                for (int neigh : graph[curr]) {
-                    if (ans.find(neigh) == ans.end()) {
-                        ans.insert(neigh);
-                        queue.push(neigh);
-                    }
+            for (int j = 0; j < temp.size(); ++j) {
+                if (find(groups, temp[j]) != 0) {
+                    groups[temp[j]] = temp[j];
                 }
             }
         }
-        return vector<int>(ans.begin(), ans.end());
+
+        vector<int> result;
+        for (int j = 0; j < n; ++j) {
+            if (find(groups, j) == 0)result.push_back(j);
+        }
+
+        return result;
     }
 };
+auto init = []() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
