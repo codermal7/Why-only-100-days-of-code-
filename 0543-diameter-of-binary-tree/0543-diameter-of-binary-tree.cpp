4450b9e9cc82b43cf496a8cd26bf7e8069208246
Anuj Kumar Pandey <83698322+codermal7@users.noreply.github.com>
2024-02-27 18:52:16 +0530

    Time: 6 ms (73.56%), Space: 18.9 MB (70.17%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
@@ -9,6 +9,7 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+#pragma GCC optimize("O3", "unroll-loops")
 class Solution {
 public:
     int diameterOfBinaryTree(TreeNode* root) {
@@ -26,3 +27,9 @@ public:
         return 1+max(lh,rh);
     }
 };
+auto init = []() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
