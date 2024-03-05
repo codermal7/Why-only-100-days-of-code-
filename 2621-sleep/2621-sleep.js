ee52a36bc5fbb8bb54e3327bf0f84f04ff951eab
Anuj Kumar Pandey <83698322+codermal7@users.noreply.github.com>
2024-03-06 01:23:33 +0530

    Time: 60 ms (16.41%), Space: 48.4 MB (66.42%) - LeetHub

---
+++ 2621-sleep/2621-sleep.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} millis
+ * @return {Promise}
+ */
+async function sleep(millis) {
+    return new Promise((delayresolve, reject) => 
+    {
+        if (typeof millis !== 'number' || isNaN(millis)) {
+        reject(new Error('Invalid argument. Expected a number.'));
+        } else {
+        setTimeout(delayresolve, millis);
+        }
+    });
+}
+
+/** 
+ * let t = Date.now()
+ * sleep(100).then(() => console.log(Date.now() - t)) // 100
+ */
