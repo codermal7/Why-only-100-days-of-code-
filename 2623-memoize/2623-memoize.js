a6c045436e921fede6f84c3602e745c023b8adce
Anuj Kumar Pandey <83698322+codermal7@users.noreply.github.com>
2024-03-06 01:21:04 +0530

    Time: 304 ms (44.43%), Space: 90.2 MB (47.56%) - LeetHub

---
+++ 2623-memoize/2623-memoize.js
@@ -0,0 +1,29 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+function memoize(fn) {
+    const cache = new Map();
+    return function(...args) {
+        const key = args.map(a => typeof a + '|' + JSON.stringify(a)).join(',');
+        if (cache.has(key)) {
+        return cache.get(key);
+        } else {
+        const result = fn(...args);
+        cache.set(key, result);
+        return result;
+        }
+    }
+}
+
+
+/** 
+ * let callCount = 0;
+ * const memoizedFn = memoize(function (a, b) {
+ *	 callCount += 1;
+ *   return a + b;
+ * })
+ * memoizedFn(2, 3) // 5
+ * memoizedFn(2, 3) // 5
+ * console.log(callCount) // 1 
+ */
