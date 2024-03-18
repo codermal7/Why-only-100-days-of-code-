c6d9b1583c155efb93dea74c84173458162fcf17
Anuj Kumar Pandey <83698322+codermal7@users.noreply.github.com>
2024-03-19 00:11:57 +0530

    Time: 49 ms (79.68%), Space: 50.3 MB (45.47%) - LeetHub

---
+++ 2695-array-wrapper/2695-array-wrapper.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {void}
+ */
+var ArrayWrapper = function(nums) {
+    this.nums = nums;
+};
+
+ArrayWrapper.prototype.valueOf = function() {
+    return this.nums.reduce((a, b) => a + b, 0);
+};
+
+ArrayWrapper.prototype.toString = function() {
+    return '[' + this.nums.toString() + ']';
+};
+
+/**
+ * const obj1 = new ArrayWrapper([1,2]);
+ * const obj2 = new ArrayWrapper([3,4]);
+ * obj1 + obj2; // 10
+ * String(obj1); // "[1,2]"
+ * String(obj2); // "[3,4]"
+ */
