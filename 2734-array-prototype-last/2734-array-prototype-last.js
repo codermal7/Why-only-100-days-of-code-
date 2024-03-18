26bb57ef14e77ef67c916b8fe0bac03b5ed78943
Anuj Kumar Pandey <83698322+codermal7@users.noreply.github.com>
2024-03-19 00:03:03 +0530

    Time: 58 ms (13.28%), Space: 48.9 MB (15.59%) - LeetHub

---
+++ 2734-array-prototype-last/2734-array-prototype-last.js
@@ -0,0 +1,13 @@
+/**
+ * @return {null|boolean|number|string|Array|Object}
+ */
+Array.prototype.last = function() {
+    if(this.length===0)
+        return -1;
+    return this.pop();
+};
+
+/**
+ * const arr = [1, 2, 3];
+ * arr.last(); // 3
+ */
