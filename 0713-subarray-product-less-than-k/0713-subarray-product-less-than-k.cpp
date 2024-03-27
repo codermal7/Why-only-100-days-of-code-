a729010a6dc3d1e3236679a8b91029033a0def4a
Anuj Kumar Pandey <83698322+codermal7@users.noreply.github.com>
2024-03-27 21:03:04 +0530

    Time: 46 ms (94.2%), Space: 63.7 MB (57.48%) - LeetHub

--- 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.cpp
+++ 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
     int numSubarrayProductLessThanK(vector<int>& nums, int k) {
-        int start = 0;
-        long prod = 1;
-        int count =0; // count of subarray prod  less than k
-        for(int end =0; end< nums.size(); end++){
-            prod *= nums[end];
-    
-           while(prod >= k && start < nums.size()){
-               prod = prod/nums[start];// divide product by nums at start pointer t reduce the prod
-                start++;//move start pointer because no longer nums at start can give us prod < k
-              }
-           if(prod < k)
-            count += end - start +1;
-      }
-    return count;
+        int left = 0, right = 0, product = 1, count = 0;
+        int n = nums.size();
+        if(k <= 1) return 0;
+        while (right < n) {
+        product *= nums[right];
+        while (product >= k) product /= nums[left++];
+        count += 1 + (right - left);
+        right++;
+        }
+        return count;
     }
 };
