54dc5369d447f17a4adcea0a7dbbad11f608547e
Anuj Kumar Pandey <83698322+codermal7@users.noreply.github.com>
2024-04-01 10:57:11 +0530

    Time: 4 ms (47.87%), Space: 7.6 MB (57.5%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -1,42 +1,18 @@
-// class Solution {
-// public:
-//     int lengthOfLastWord(string s) {
-//         int c=0, n=s.size();
-//         string word = "";
-//         for (int i = 0; i < n; i++) 
-//         {
-//             if (str[i] == ' ') {
-//                 word = "";
-//                 continue;
-//             }
-//             if (str[i] == ' ' && i==(n-1)) {
-//                 return word.length();
-//                 continue;
-//             }
-//             else {
-//                 word += str[i];
-//             }
-//         }
-//         return word.size();
-//     }
-// };
-
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        int length = 0;
-        bool counting = false;
+        int c = 0; bool charfound = false;
         
-        for (int i = s.length() - 1; i >= 0; i--) {
+        for (int i = s.size() - 1; i >= 0; i--) {
             if (s[i] != ' ') {
-                counting = true;
-                length++;
+                charfound = true;
+                c++;
             }
-            else if (counting==true) {
+            else if(charfound==true) {
                 break;
             }
         }
         
-        return length;
+        return c;
     }
 };
