ee37d32d1067e83a3e06551c13b998b82db3e869
Anuj Kumar Pandey <83698322+codermal7@users.noreply.github.com>
2024-04-04 15:54:21 +0530

    Time: 0 ms (100%), Space: 7.4 MB (78.96%) - LeetHub

--- 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
@@ -1,36 +1,33 @@
-// class Solution {
-// public:
-//     int maxDepth(string s) {
-//         int ans = 0;
-//         int openBrackets = 0;
-
-//         for (char c : s) {
-//             if (c == '(') {
-//                 openBrackets++;
-//             } else if (c == ')') {
-//                 openBrackets--;
-//             }
-
-//             ans = max(ans, openBrackets);
-//         }
-
-//         return ans;
-//     }
-// };
-
 class Solution {
 public:
     int maxDepth(string s) {
         int ans = 0;
-        stack<char> st;
+        int openP = 0;
         for (char c : s) {
             if (c == '(') {
-                st.push(c);
+                openP++;
             } else if (c == ')') {
-                st.pop();
+                openP--;
             }
-            ans = max(ans, (int)st.size());
+            ans = max(ans, openP);
         }
         return ans;
     }
 };
+
+// class Solution {
+// public:
+//     int maxDepth(string s) {
+//         int ans = 0;
+//         stack<char> st;
+//         for (char c : s) {
+//             if (c == '(') {
+//                 st.push(c);
+//             } else if (c == ')') {
+//                 st.pop();
+//             }
+//             ans = max(ans, (int)st.size());
+//         }
+//         return ans;
+//     }
+// };
