8e487743bd700feb549922da2ecdbcb6f348aa79
Anuj Kumar Pandey <83698322+codermal7@users.noreply.github.com>
2024-04-04 15:54:06 +0530

    Time: 0 ms (100%), Space: 7.7 MB (6.33%) - LeetHub

--- 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
@@ -1,19 +1,36 @@
+// class Solution {
+// public:
+//     int maxDepth(string s) {
+//         int ans = 0;
+//         int openBrackets = 0;
+
+//         for (char c : s) {
+//             if (c == '(') {
+//                 openBrackets++;
+//             } else if (c == ')') {
+//                 openBrackets--;
+//             }
+
+//             ans = max(ans, openBrackets);
+//         }
+
+//         return ans;
+//     }
+// };
+
 class Solution {
 public:
     int maxDepth(string s) {
         int ans = 0;
-        int openBrackets = 0;
-
+        stack<char> st;
         for (char c : s) {
             if (c == '(') {
-                openBrackets++;
+                st.push(c);
             } else if (c == ')') {
-                openBrackets--;
+                st.pop();
             }
-            
-            ans = max(ans, openBrackets);
+            ans = max(ans, (int)st.size());
         }
-        
         return ans;
     }
 };
