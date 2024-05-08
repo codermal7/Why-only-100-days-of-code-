b8e6e7dfd612c1313ca2dc8528b029994fb0815e
Anuj Kumar Pandey <83698322+codermal7@users.noreply.github.com>
2024-05-08 23:39:50 +0530

    Time: 10 ms (51.87%), Space: 14.3 MB (62.46%) - LeetHub

---
+++ 0506-relative-ranks/0506-relative-ranks.cpp
@@ -0,0 +1,36 @@
+class Solution {
+private:
+    int findMax(vector<int>& score) {
+        int maxScore = 0;
+        for (int s : score) {
+            if (s > maxScore) {
+                maxScore = s;
+            }
+        }
+        return maxScore;
+    }
+public:
+    vector<string> findRelativeRanks(vector<int>& score) {
+        int N = score.size();
+        int M = findMax(score);
+        vector<int> scoreToIndex(M + 1, 0);
+        for (int i = 0; i < N; i++) {
+            scoreToIndex[score[i]] = i + 1;
+        }
+        const vector<string> MEDALS = {"Gold Medal", "Silver Medal", "Bronze Medal"};
+        vector<string> rank(N);
+        int place = 1;
+        for (int i = M; i >= 0; i--) {
+            if (scoreToIndex[i] != 0) {
+                int originalIndex = scoreToIndex[i] - 1;
+                if (place < 4) {
+                    rank[originalIndex] = MEDALS[place - 1];
+                } else {
+                    rank[originalIndex] = to_string(place);
+                }
+                place++;
+            }
+        }
+        return rank;
+    }
+};
