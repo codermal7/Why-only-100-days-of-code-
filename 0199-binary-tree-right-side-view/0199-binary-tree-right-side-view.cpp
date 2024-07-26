0cff516f1cd2b9452e974839d6d89a1e18cc11aa
Anuj Kumar Pandey <83698322+codermal7@users.noreply.github.com>
2024-07-26 10:21:13 +0530

    Time: 3 ms (57.08%), Space: 12.3 MB (100%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -12,21 +12,36 @@
 class Solution {
 public:
     vector<int> rightSideView(TreeNode* root) {
-        vector<int> res;
-        if(root==NULL) return res;
-        queue<TreeNode*> q;
-        q.push(root);
-        while(!q.empty()){
-            TreeNode *temp = q.front();
-            res.push_back(temp->val);
-            int size = q.size();
-            for(int i=0;i<size;i++){
-                TreeNode *curr = q.front();
-                q.pop();
-                if(curr->right!=NULL) q.push(curr->right);
-                if(curr->left!=NULL) q.push(curr->left);
-            }
-        }
-        return res;
+        vector<int> ans;
+        // if (root == NULL) return ans;
+        // queue<pair<TreeNode*, int>> q;
+        // q.push({root,0});
+        // map<int,int> mp;
+        // while(!q.empty())
+        // {
+        //     auto it=q.front();
+        //     q.pop();
+        //     TreeNode* node=it.first;
+        //     int line=it.second;
+        //     mp[line]=node->val;
+        //     if(node->left != NULL)
+        //     q.push({node->left,line+1});
+        //     if(node->right != NULL)
+        //     q.push({node->right,line+1});
+        // }
+        // for(auto i:mp)
+        // ans.push_back(i.second);
+        // return ans;
+        if(root==NULL) return ans;
+        rightView(ans,root,0);
+        return ans;
+    }
+    void rightView(vector<int> &ans,TreeNode* root,int line)
+    {
+        if(root==NULL) return;
+        if(line==ans.size())
+        ans.push_back(root->val);
+        rightView(ans,root->right,line+1);
+        rightView(ans,root->left,line+1);
     }
 };
