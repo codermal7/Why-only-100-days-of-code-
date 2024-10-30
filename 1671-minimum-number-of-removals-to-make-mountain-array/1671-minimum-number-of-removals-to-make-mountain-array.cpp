8cda04f02d211e97b93c96869819429c59ef4094
Anuj Kumar Pandey <83698322+codermal7@users.noreply.github.com>
2024-10-30 19:03:44 +0530

    Time: 60 ms (50%), Space: 14.7 MB (94.12%) - LeetHub

---
+++ 1671-minimum-number-of-removals-to-make-mountain-array/1671-minimum-number-of-removals-to-make-mountain-array.cpp
@@ -0,0 +1,38 @@
+#pragma GCC optimize("O3")
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops", "no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+static const auto DPSolver = [](){ 
+    std::ios_base::sync_with_stdio(false); 
+    std::cin.tie(nullptr); 
+    std::cout.tie(nullptr); 
+    return 'c'; 
+}();
+
+class Solution {
+public:
+    int minimumMountainRemovals(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> LIS(n, 1), LDS(n, 1);       
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < i; ++j) {
+                if (nums[i] > nums[j]) {
+                    LIS[i] = max(LIS[i], LIS[j] + 1);
+                }
+            }
+        }
+        for (int i = n - 1; i >= 0; --i) {
+            for (int j = n - 1; j > i; --j) {
+                if (nums[i] > nums[j]) {
+                    LDS[i] = max(LDS[i], LDS[j] + 1);
+                }
+            }
+        }
+        int maxi = 0;       
+        for (int i = 1; i < n - 1; ++i) {
+            if (LIS[i] > 1 && LDS[i] > 1) {  
+                maxi = max(maxi, LIS[i] + LDS[i] - 1);
+            }
+        }
+        return n - maxi;
+    }
+};
