d3f2e1e9a74a4aa95a458686895f4872c4ab0b81
Anuj Kumar Pandey <83698322+codermal7@users.noreply.github.com>
2025-01-02 00:51:06 +0530

    Time: 3 ms (18.71%), Space: 8.7 MB (38.26%) - LeetHub

--- 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.cpp
+++ 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.cpp
@@ -1,46 +1,22 @@
-// class Solution {
-// public:
-//     int maxScore(string s) {
-//         int l=s.size();int maxi=0;
-//         for(int i=0;i<l;i++)
-//         {
-//             int c=0;
-//             for(int j=0;j<=i;j++)
-//             {
-//                 if(s[j]=='0')
-//                 c++;
-//             }
-//             for(int j=i+1;j<l;j++)
-//             {
-//                 if(s[j]=='1')
-//                 c++;
-//             }
-//             maxi=max(maxi,c);
-//         }
-//         return maxi;
-//     }
-// };
 class Solution {
 public:
     int maxScore(string s) {
-        int ans = 0;
-        for (int i = 0; i < s.size() - 1; i++) {
-            int curr = 0;
-            for (int j = 0; j <= i; j++) {
-                if (s[j] == '0') {
-                    curr++;
-                }
+        int l=s.size();int maxi=0;
+        for(int i=0;i<l-1;i++)
+        {
+            int c=0;
+            for(int j=0;j<=i;j++)
+            {
+                if(s[j]=='0')
+                c++;
             }
-            
-            for (int j = i + 1; j < s.size(); j++) {
-                if (s[j] == '1') {
-                    curr++;
-                }
+            for(int j=i+1;j<l;j++)
+            {
+                if(s[j]=='1')
+                c++;
             }
-            
-            ans = max(ans, curr);
+            maxi=max(maxi,c);
         }
-        
-        return ans;
+        return maxi;
     }
 };
