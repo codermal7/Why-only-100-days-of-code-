55390d6bc34640682a94517a7ef90790826a2a2d
Anuj Kumar Pandey <83698322+codermal7@users.noreply.github.com>
2025-01-02 00:51:53 +0530

    Time: 6 ms (7.45%), Space: 8.7 MB (38.26%) - LeetHub

--- 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.cpp
+++ 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int maxScore(string s) {
-        int l=s.size();int maxi=0;
-        for(int i=0;i<l-1;i++)
+        int maxi=0;
+        for(int i=0;i<s.size()-1;i++)
         {
             int c=0;
             for(int j=0;j<=i;j++)
@@ -10,7 +10,7 @@ public:
                 if(s[j]=='0')
                 c++;
             }
-            for(int j=i+1;j<l;j++)
+            for(int j=i+1;j<s.size();j++)
             {
                 if(s[j]=='1')
                 c++;
