4b4c950a876d02861eb198ac002efefc9bc94fb0
Anuj Kumar Pandey <83698322+codermal7@users.noreply.github.com>
2025-02-21 02:23:17 +0530

    Time: 6 ms (8.68%), Space: 7.4 MB (100%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,26 +1,20 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        if(s.length()<=1)
-        return true;
-        string str="";
-        for(auto it:s)
+        int l=0,r=s.length()-1;
+        while(l<r)
         {
-            if(isalnum(it))
-            str+=tolower(it);
-        }
-        // cout<<str<<endl;
-        int low=0,high=str.length()-1;
-        // cout<<high<<endl;
-        int mid=low+((high - low) / 2);
-        // cout<<mid<<endl;
-        while(low<high)
-        {
-            // cout<<"low :"<<str[low]<<", high :"<<str[high]<<endl;
-            if(str[low]!=str[high])
-            return false;
-            low++;
-            high--;
+            if(!isalnum(s[l]))
+                l++;
+            else if(!isalnum(s[r]))
+                r--;
+            else if(tolower(s[l])!=tolower(s[r]))
+                return false;
+            else
+            {
+                l++;r--;
+            }
+                
         }
         return true;
     }
