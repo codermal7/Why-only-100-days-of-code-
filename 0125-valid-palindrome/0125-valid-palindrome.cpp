b65bfe27f02a43af2d551b20408c09ac7402b0d3
Anuj Kumar Pandey <83698322+codermal7@users.noreply.github.com>
2025-02-21 02:23:44 +0530

    Time: 0 ms (100%), Space: 9.9 MB (78.99%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,3 +1,31 @@
+// class Solution {
+// public:
+//     bool isPalindrome(string s) {
+//         if(s.length()<=1)
+//         return true;
+//         string str="";
+//         for(auto it:s)
+//         {
+//             if(isalnum(it))
+//             str+=tolower(it);
+//         }
+//         // cout<<str<<endl;
+//         int low=0,high=str.length()-1;
+//         // cout<<high<<endl;
+//         int mid=low+((high - low) / 2);
+//         // cout<<mid<<endl;
+//         while(low<high)
+//         {
+//             // cout<<"low :"<<str[low]<<", high :"<<str[high]<<endl;
+//             if(str[low]!=str[high])
+//             return false;
+//             low++;
+//             high--;
+//         }
+//         return true;
+//     }
+// };
+
 class Solution {
 public:
     bool isPalindrome(string s) {
