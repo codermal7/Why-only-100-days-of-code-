408a82f6527d9f3b7002ba37de3c92029b345de2
Anuj Kumar Pandey <83698322+codermal7@users.noreply.github.com>
2025-03-01 00:38:07 +0530

    Time: 0 ms (100%), Space: 15.5 MB (92.36%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,27 +1,55 @@
+// class Solution {
+// public:
+//     void nextPermutation(vector<int>& nums) {
+//         int n=nums.size(),index=-1;
+//         if(n==1 || n==0) return;
+//         for(int i=n-2;i>=0;i--)
+//         {
+//             if(nums[i]<nums[i+1])
+//             {
+//                 index=i;break;
+//             }
+//         }
+//         if(index==-1)
+//         reverse(nums.begin(),nums.end());
+//         else{
+//             for(int i=n-1;i>index;i--)
+//             {
+//                 if(nums[i]>nums[index])
+//                 {
+//                     swap(nums[i],nums[index]);break;
+//                 }
+//             }
+//             reverse(nums.begin()+index+1,nums.end());
+//         }
+//         // next_permutation(nums.begin(),nums.end());
+//     }
+// };
+
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        int n=nums.size(),index=-1;
-        if(n==1 || n==0) return;
-        for(int i=n-2;i>=0;i--)
+        int n=nums.size(), index=-1;
+        for(int i=n-1;i>0;i--)
         {
-            if(nums[i]<nums[i+1])
+            if(nums[i-1]<nums[i])
             {
-                index=i;break;
+                index=i-1;
+                break;
             }
         }
-        if(index==-1)
-        reverse(nums.begin(),nums.end());
-        else{
+        cout<<index<<endl;
+        if(index!=-1)
+        {
             for(int i=n-1;i>index;i--)
             {
                 if(nums[i]>nums[index])
                 {
-                    swap(nums[i],nums[index]);break;
+                    swap(nums[i],nums[index]);
+                    break;
                 }
             }
-            reverse(nums.begin()+index+1,nums.end());
         }
-        // next_permutation(nums.begin(),nums.end());
+        reverse(nums.begin()+index+1,nums.end());
     }
 };
