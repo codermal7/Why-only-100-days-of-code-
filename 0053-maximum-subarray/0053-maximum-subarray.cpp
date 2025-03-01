c29de9b07e5af7b430d5dcfdf8e91e1be4f2de51
Anuj Kumar Pandey <83698322+codermal7@users.noreply.github.com>
2025-03-01 23:44:11 +0530

    Time: 0 ms (100%), Space: 71.8 MB (18.23%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -10,5 +10,34 @@ public:
             sum=0;
         }
         return maxi;
+
+        // My code for also printing the maximum subarray sum
+        /*
+        int n = nums.size();
+        int sum = 0, maxSum = INT_MIN;
+        int start = 0, end = 0, s = 0;
+
+        for (int i = 0; i < n; i++) {
+            sum += nums[i];
+
+            if (sum > maxSum) { 
+                maxSum = sum;
+                start = s;
+                end = i;
+            }
+
+            if (sum < 0) {
+                sum = 0;
+                s = i + 1;
+            }
+        }
+
+        cout << "Maximum Subarray Sum: ";
+        for (int i = start; i <= end; i++) {
+            cout << nums[i] << " ";
+        }
+        cout << endl;
+        return maxSum;
+        */
     }
 };
