8bc7221ff1409a32f04709dd3e3bcfa1d4a356d7
Anuj Kumar Pandey <83698322+codermal7@users.noreply.github.com>
2025-03-04 02:24:58 +0530

    Time: 0 ms (100%), Space: 16.7 MB (65.87%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,3 +1,59 @@
+// class Solution {
+// public:
+//     int orangesRotting(vector<vector<int>>& grid) {
+//         int n=grid.size();
+//         int m=grid[0].size();
+//         int cntfresh=0;
+//         queue<pair< pair<int,int>, int>> q;
+//         int vis[n][m];
+//         // vector<vector<int>> vis(n, vector<int>(m, 0));
+//         for(int i=0;i<n;i++)
+//         {
+//             for(int j=0;j<m;j++)
+//             {
+//                 if(grid[i][j]==2)
+//                 {
+//                     q.push({{i,j},0});
+//                     vis[i][j]=2;
+//                 }
+//                 else if(grid[i][j]==1)
+//                 cntfresh++;
+//                 else
+//                 vis[i][j]=0;
+//             }
+//         }
+//         int tm=0,cnt=0;
+//         int drow[4]={-1,0,1,0};
+//         int dcol[4]={0,1,0,-1};
+
+//         while(!q.empty())
+//         {
+//             int r=q.front().first.first;
+//             int c=q.front().first.second;
+//             int t=q.front().second;
+//             tm=max(tm,t);
+//             q.pop();
+//             for(int i=0;i<4;i++)
+//             {
+//                 int nrow=r+drow[i];
+//                 int ncol=c+dcol[i];
+//                 if(nrow>=0 && nrow<n && ncol>=0 && ncol<m 
+//                 && vis[nrow][ncol]!=2 && grid[nrow][ncol]==1)
+//                 {
+//                     q.push({{nrow,ncol},t+1});
+//                     vis[nrow][ncol]=2;
+//                     cnt++;
+//                 }
+//             }
+//         }
+
+//         if(cnt!=cntfresh)
+//         return -1;
+
+//         return tm;
+//     }
+// };
+
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
