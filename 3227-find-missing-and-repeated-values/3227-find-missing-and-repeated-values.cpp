0f24cf81f714f63b4c64f11dc4dece3e95250f6e
Anuj Kumar Pandey <83698322+codermal7@users.noreply.github.com>
2025-03-07 04:19:31 +0530

    Time: 0 ms (100%), Space: 26.2 MB (89.76%) - LeetHub

--- 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.cpp
+++ 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.cpp
@@ -41,37 +41,26 @@ public:
         // 45;
         // 45-9=36;
 
-        // const int n=arr.size(), n2=n*n;
-        // vector<unsigned char> freq(n2+1, 0);
-        // for(int i=0; i<n; i++){
-        //     for(int j=0; j<n; j++){
-        //         freq[arr[i][j]]++;
-        //     }
-        // }
-        // int a=0, b=0;
-        // for(int i=1; i<=n2; i++){
-        //     if (freq[i]==2) a=i;
-        //     else if (freq[i]==0) b=i;
-        // }
-        // return {a, b};
-
-        int a = 0 , b = 0;
-        int n = arr.size();
-        int m = n * n;
-        int totalSum = (m * (m+1))/2;
-        int sum = 0;
-        unordered_set<int> nums;
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
-                if (nums.count(arr[i][j])) 
-                a=arr[i][j];
-                else{
-                    nums.insert(arr[i][j]);
-                    sum += arr[i][j];
-                }
+        const int n=arr.size(), n2=n*n;
+        vector<unsigned char> freq(n2+1, 0);
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                freq[arr[i][j]]++;
             }
         }
-        b = totalSum - sum;
-        return {a,b};
+        int a=0, b=0;
+        for(int i=1; i<=n2; i++){
+            if (freq[i]==2) a=i;
+            else if (freq[i]==0) b=i;
+        }
+        return {a, b};
     }
 };
+
+auto init = []()
+{ 
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
