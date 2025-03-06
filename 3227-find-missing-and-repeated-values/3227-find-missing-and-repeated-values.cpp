e29ebd285290ed09cc478005ec5970011e5979ac
Anuj Kumar Pandey <83698322+codermal7@users.noreply.github.com>
2025-03-07 04:40:05 +0530

    Time: 0 ms (100%), Space: 25.7 MB (91.83%) - LeetHub

--- 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.cpp
+++ 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.cpp
@@ -1,78 +1,23 @@
 class Solution {
 public:
     vector<int> findMissingAndRepeatedValues(vector<vector<int>>& arr) {
-        // int n=arr.size(),a,b;
-        // unordered_map<int,int> mp;
-        
-        // for (int i = 1; i <= n*n; i++)
-        // {
-        //     mp[i] = 0;
-        // }
-
-        // for(int i=0;i<n;i++)
-        // {
-        //     for(int j=0;j<n;j++)
-        //     {
-        //         mp[arr[i][j]]++;
-        //     }
-        // }
-
-        // for(auto it:mp)
-        // {
-        //     if(it.second>1)
-        //     a=it.first;
-        // }
-        // for(auto it:mp)
-        // {
-        //     if(it.second<1)
-        //     b=it.first;
-        // }
-        // return {a,b};
-        
-        // Rough mathematical formula guessing.
-        
-        // n=4;
-        // sum=(n*(n+1))/2;
-        // 10;
-        // 10-2=8;
-
-        // n=9;
-        // sum=(n*(n+1))/2;
-        // 45;
-        // 45-9=36;
-
-        // const int n=arr.size(), n2=n*n;
-        // vector<unsigned char> freq(n2+1, 0);
-        // for(int i=0; i<n; i++){
-        //     for(int j=0; j<n; j++){
-        //         freq[arr[i][j]]++;
-        //     }
-        // }
-        // int a=0, b=0;
-        // for(int i=1; i<=n2; i++){
-        //     if (freq[i]==2) a=i;
-        //     else if (freq[i]==0) b=i;
-        // }
-        // return {a, b};
-
-        int a = 0 , b = 0;
-        const int n = arr.size();
-        const int m = n * n;
-        const int totalSum = (m * (m+1))/2;
-        int sum = 0;
-        unordered_set<int> nums;
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
-                if (nums.count(arr[i][j])) 
-                a=arr[i][j];
-                else{
-                    nums.insert(arr[i][j]);
-                    sum += arr[i][j];
-                }
+        int n = arr.size();
+        long long sum = 0, sum_sq = 0;
+        for (auto& row : arr) {
+            for (int num : row) {
+                sum += num;
+                sum_sq += (long long)num * num;
             }
         }
-        b = totalSum - sum;
-        return {a,b};
+        long long n_sq = (long long)n * n;
+        long long S = n_sq * (n_sq + 1) / 2;
+        long long S_sq = n_sq * (n_sq + 1) * (2 * n_sq + 1) / 6;
+        long long delta_sum = sum - S;
+        long long delta_sq_sum = sum_sq - S_sq;
+        long long a_plus_b = delta_sq_sum / delta_sum;
+        int a = (delta_sum + a_plus_b) / 2;
+        int b = (a_plus_b - delta_sum) / 2;
+        return {a, b};
     }
 };
 
