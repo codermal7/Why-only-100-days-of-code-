55148bc92d17478036ca7d961accc4ba49cda9c2
Anuj Kumar Pandey <83698322+codermal7@users.noreply.github.com>
2025-03-07 04:40:32 +0530

    Time: 0 ms (100%), Space: 25.8 MB (90.25%) - LeetHub

--- 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.cpp
+++ 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.cpp
@@ -2,21 +2,25 @@ class Solution {
 public:
     vector<int> findMissingAndRepeatedValues(vector<vector<int>>& arr) {
         int n = arr.size();
-        long long sum = 0, sum_sq = 0;
-        for (auto& row : arr) {
-            for (int num : row) {
-                sum += num;
-                sum_sq += (long long)num * num;
+        long long tN = n * n;
+        long long eS = (long long)tN * (tN + 1) / 2;
+        long long eSqS = (long long)tN * (tN + 1) * (2 * tN + 1) / 6;
+
+        long long aS = 0, aSqS = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                int v = arr[i][j];
+                aS += v;
+                aSqS += (long long)v * v;
             }
         }
-        long long n_sq = (long long)n * n;
-        long long S = n_sq * (n_sq + 1) / 2;
-        long long S_sq = n_sq * (n_sq + 1) * (2 * n_sq + 1) / 6;
-        long long delta_sum = sum - S;
-        long long delta_sq_sum = sum_sq - S_sq;
-        long long a_plus_b = delta_sq_sum / delta_sum;
-        int a = (delta_sum + a_plus_b) / 2;
-        int b = (a_plus_b - delta_sum) / 2;
+        
+        long long d = aS - eS;
+        long long s_ab = (aSqS - eSqS) / d;
+
+        int a = (int)((d + s_ab) / 2);
+        int b = (int)(s_ab - a);
+
         return {a, b};
     }
 };
